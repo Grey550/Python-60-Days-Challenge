852b559354e8fff7ddbc28b7f9806487c51f6b28
Angie <angie.monnye@gmail.com>
2023-05-06 15:22:34 +0200

    Section 5: ToDo List App

---
+++ DayFive/codingExerciseTwo.py
@@ -0,0 +1,9 @@
+# ips = ['100.122.133.105', '100.122.133.111']
+#
+# ip = int(input("Enter the index of the IP you want: "))
+# print("You chose", ips[ip])
+
+menu = ["pasta", "pizza", "salad"]
+
+for i, j in enumerate(menu):
+    print(f"{i}.{j}")
